b613f6bd8458ba07fba4fead8e410d766d2e17eb
ttackcots <117217182+orsoneth@users.noreply.github.com>
2025-04-30 18:14:50 +0500

    Add files via upload

---
+++ dependencies.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Preparing dependencies installation . . .
+echo Installing dependencies . . .
+pip install discord
+pip install requests
+pip install asyncio
+Dependencies installed. 
+timout /t 1
+cls
+echo All dependencies are installed.
